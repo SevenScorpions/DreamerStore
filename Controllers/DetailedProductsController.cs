685723147f1f7f3734845690d683065dc244562e
SevenScorpions <huyhoathangbmt@gmail.com>
2023-11-07 23:01:05 +0700

    .

---
+++ Controllers/DetailedProductsController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DreamerStore2.Models;
+
+namespace DreamerStore2.Controllers
+{
+    public class DetailedProductsController : Controller
+    {
+        private readonly SonungvienContext _context;
+
+        public DetailedProductsController(SonungvienContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DetailedProducts
+        public async Task<IActionResult> Index(string? id)
+        {
+            var product = await _context.Products.Where(p=>p.Meta==id).FirstOrDefaultAsync();
+            if (product == null)
+            {
+                return NotFound();
+            }
+            var sonungvienContext = _context.DetailedProducts.Include(d => d.Product);
+            return View(await sonungvienContext.Where(d=>d.ProductId==product.ProductId).ToListAsync());
+        }
+
+        // GET: DetailedProducts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.DetailedProducts == null)
+            {
+                return NotFound();
+            }
+
+            var detailedProduct = await _context.DetailedProducts
+                .Include(d => d.Product)
+                .FirstOrDefaultAsync(m => m.DetailedProductId == id);
+            if (detailedProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(detailedProduct);
+        }
+
+        // GET: DetailedProducts/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId");
+            return View();
+        }
+
+        // POST: DetailedProducts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DetailedProductId,DetailedProductPrice,DetailedProductQuantity,DetailedProductName,ProductId,Order,Meta,Image,Hide,CreatedAt,UpdatedAt")] DetailedProduct detailedProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(detailedProduct);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", detailedProduct.ProductId);
+            return View(detailedProduct);
+        }
+
+        // GET: DetailedProducts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.DetailedProducts == null)
+            {
+                return NotFound();
+            }
+
+            var detailedProduct = await _context.DetailedProducts.FindAsync(id);
+            if (detailedProduct == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", detailedProduct.ProductId);
+            return View(detailedProduct);
+        }
+
+        // POST: DetailedProducts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DetailedProductId,DetailedProductPrice,DetailedProductQuantity,DetailedProductName,ProductId,Order,Meta,Image,Hide,CreatedAt,UpdatedAt")] DetailedProduct detailedProduct)
+        {
+            if (id != detailedProduct.DetailedProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(detailedProduct);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DetailedProductExists(detailedProduct.DetailedProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", detailedProduct.ProductId);
+            return View(detailedProduct);
+        }
+
+        // GET: DetailedProducts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.DetailedProducts == null)
+            {
+                return NotFound();
+            }
+
+            var detailedProduct = await _context.DetailedProducts
+                .Include(d => d.Product)
+                .FirstOrDefaultAsync(m => m.DetailedProductId == id);
+            if (detailedProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(detailedProduct);
+        }
+
+        // POST: DetailedProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.DetailedProducts == null)
+            {
+                return Problem("Entity set 'SonungvienContext.DetailedProducts'  is null.");
+            }
+            var detailedProduct = await _context.DetailedProducts.FindAsync(id);
+            if (detailedProduct != null)
+            {
+                _context.DetailedProducts.Remove(detailedProduct);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DetailedProductExists(int id)
+        {
+          return (_context.DetailedProducts?.Any(e => e.DetailedProductId == id)).GetValueOrDefault();
+        }
+    }
+}
